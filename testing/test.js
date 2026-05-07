bdfbb988b5e17e9e7cad0ada34008e40f3b89df0
Luis <luisagp2005@gmail.com>
2026-05-06 18:36:49 -0600

    Nuevas modificaciones de testing automatico

--- testing/test.js
+++ testing/test.js
@@ -1,20 +1,59 @@
-const { Builder, By, Key, until } = require('selenium-webdriver');
+const { Builder, By, until } = require('selenium-webdriver');
 
-(async function () {
-  let driver = await new Builder().forBrowser('chrome').build();
+async function loginTest() {
 
-  try {
-    await driver.get('https://www.google.com');
+    // Crear navegador
+    let driver = await new Builder().forBrowser('chrome').build();
 
-    let input = await driver.findElement(By.name('q'));
-    await input.sendKeys('Hola Selenium', Key.RETURN);
+    try {
 
-    await driver.wait(until.titleContains('Selenium'), 5000);
+        // Abrir página
+        await driver.get('http://localhost:5173/login#');
 
-    console.log("Funciona 👍");
-  } catch (e) {
-    console.log("Error:", e);
-  } finally {
-    await driver.quit();
-  }
-})();
+        // Maximizar ventana
+        await driver.manage().window().maximize();
+
+        // Esperar inputs
+        await driver.wait(until.elementLocated(By.css('input[type="email"]')), 5000);
+
+        // Escribir email
+        await driver.findElement(By.css('input[type="email"]'))
+            .sendKeys('l22056714@saltillo.tecnm.mx');
+
+        // Escribir password
+        await driver.findElement(By.css('input[type="password"]'))
+            .sendKeys('12345');
+
+        // Click botón ingresar
+        await driver.findElement(By.css('button[type="submit"]'))
+            .click();
+
+        // Esperar mensaje de error
+        const errorDiv = await driver.wait(
+            until.elementLocated(
+                By.xpath("//div[contains(text(),'Usuario o contraseña incorrectos')]")
+            ),
+            5000
+        );
+
+        // Verificar si se mostró
+        if (await errorDiv.isDisplayed()) {
+            console.log("Funcionamiento correcto");
+        }
+
+    } catch (error) {
+
+        console.error("Error en la prueba:");
+        console.error(error);
+
+    } finally {
+
+        // Esperar un poco antes de cerrar
+        await driver.sleep(3000);
+
+        // Cerrar navegador
+        await driver.quit();
+    }
+}
+
+loginTest();
